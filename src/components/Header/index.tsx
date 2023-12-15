d94260bf3380df646f6119ac9ce4ec19a01d04c7
Lucas Mazini <31120887+LukasMM@users.noreply.github.com>
2023-12-15 09:54:05 -0300

    Add files via upload

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
+
+import { useCart } from '../../hooks/useCart'
+import { Aside, Container } from './styles'
+
+import Logo from '../../assets/logo.svg'
+
+export function Header() {
+  const { cart } = useCart()
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={Logo} alt="Coffee Delivery" />
+      </Link>
+
+      <Aside>
+        <div>
+          <MapPin size={22} weight="fill" />
+          <span>São Paulo, SP</span>
+        </div>
+
+        <Link to={`cart`} aria-disabled={cart.length === 0}>
+          <ShoppingCart size={22} weight="fill" />
+          {cart.length > 0 ? <span>{cart.length}</span> : null}
+        </Link>
+      </Aside>
+    </Container>
+  )
+}
